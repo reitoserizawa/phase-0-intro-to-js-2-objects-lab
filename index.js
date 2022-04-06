a41fea807dddc890bfeba27bcd98c6835d8bd703
ReitoSerizawa <s.reitiger@gmail.com>
2022-04-06 12:17:01 -0400

    commit tree 634c363266f2a23fd599cd93c9bbc7890a4f655f

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Reito Serizawa',
+    streetAddress: '444 Warrent Street',
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {
+        ...obj, [key]: value
+    }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value
+    return obj
+}
+
+function deleteFromEmployeeByKey (obj, key) {
+    const newObj = {...obj}
+    delete newObj[key]
+    return newObj
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj,key) {
+    delete obj[key]
+    return obj
+}
